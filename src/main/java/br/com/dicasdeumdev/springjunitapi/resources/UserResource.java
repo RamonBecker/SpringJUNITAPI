ee00673c7c1a73e0ed2c361db2b90883e832b479
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-01-10 19:52:14 -0300

    Endpoint findById

---
+++ src/main/java/br/com/dicasdeumdev/springjunitapi/resources/UserResource.java
@@ -0,0 +1,18 @@
+package br.com.dicasdeumdev.springjunitapi.resources;
+
+
+import br.com.dicasdeumdev.springjunitapi.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping (value = "/user")
+public class UserResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(new User(1, "Ramon", "becker@gmail.com", "1234"));
+    }
+}
