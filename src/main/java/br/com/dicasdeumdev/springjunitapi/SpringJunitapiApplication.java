5915662105d9af887b03b6fb408d1ca5d2bde3ab
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-01-10 19:15:03 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/dicasdeumdev/springjunitapi/SpringJunitapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.springjunitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJunitapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJunitapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dicasdeumdev/springjunitapi/SpringJunitapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.springjunitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJunitapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
