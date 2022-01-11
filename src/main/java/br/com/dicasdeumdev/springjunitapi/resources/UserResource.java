b1f00effe19dfff33cf669330d900c3871d874c2
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-01-11 17:20:19 -0300

    retornando dto para user resource

--- src/main/java/br/com/dicasdeumdev/springjunitapi/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/springjunitapi/resources/UserResource.java
@@ -1,8 +1,9 @@
 package br.com.dicasdeumdev.springjunitapi.resources;
 
 
-import br.com.dicasdeumdev.springjunitapi.domain.User;
+import br.com.dicasdeumdev.springjunitapi.domain.dto.UserDTO;
 import br.com.dicasdeumdev.springjunitapi.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
