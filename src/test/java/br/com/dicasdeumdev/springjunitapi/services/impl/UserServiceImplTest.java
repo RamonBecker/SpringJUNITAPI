150bdda39ab5e1b22f8cb61313f1a0beaec21142
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-01-12 16:30:50 -0300

    Teste para a classe UserService método: delete com sucesso

--- src/test/java/br/com/dicasdeumdev/springjunitapi/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/springjunitapi/services/impl/UserServiceImplTest.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -142,6 +142,15 @@ class UserServiceImplTest {
             assertEquals(DataIntegratyViolationException.class, ex.getClass());
             assertEquals(E_MAIL_JA_CADASTRADO, ex.getMessage());
         }
+    }
+
+    @Test
+    void deleteWithSuccess(){
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
     private void startUser(){
