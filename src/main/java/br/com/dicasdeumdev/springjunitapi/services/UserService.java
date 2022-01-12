52b90a591e26f5c1e22cc26e17f94083b529a5b9
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-01-12 10:16:29 -0300

    Endpoint create para user

--- src/main/java/br/com/dicasdeumdev/springjunitapi/services/UserService.java
+++ src/main/java/br/com/dicasdeumdev/springjunitapi/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.dicasdeumdev.springjunitapi.services;
 
 import br.com.dicasdeumdev.springjunitapi.domain.User;
+import br.com.dicasdeumdev.springjunitapi.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ public interface UserService {
 
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/br/com/dicasdeumdev/springjunitapi/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/springjunitapi/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package br.com.dicasdeumdev.springjunitapi.services.impl;
 
 import br.com.dicasdeumdev.springjunitapi.domain.User;
+import br.com.dicasdeumdev.springjunitapi.domain.dto.UserDTO;
 import br.com.dicasdeumdev.springjunitapi.repositories.UserRepository;
 import br.com.dicasdeumdev.springjunitapi.services.UserService;
 import br.com.dicasdeumdev.springjunitapi.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
@@ -25,6 +30,11 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj, User.class));
     }
 }
 
